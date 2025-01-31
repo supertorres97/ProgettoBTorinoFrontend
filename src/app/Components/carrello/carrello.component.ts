1bb9ebf440e9f156b7ac7f4f14b87e15187b9b6f
danielpaliuc <daniel.paliuc16@gmail.com>
2025-01-31 16:38:20 +0100

    creazione component carrello

---
+++ src/app/Components/carrello/carrello.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrello',
+  standalone: false,
+  
+  templateUrl: './carrello.component.html',
+  styleUrl: './carrello.component.css'
+})
+export class CarrelloComponent {
+
+}
