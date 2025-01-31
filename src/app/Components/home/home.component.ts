e2af530e0002a15e324e77c57ab1c42455d57bb8
danielpaliuc <daniel.paliuc16@gmail.com>
2025-01-31 16:37:44 +0100

    creazione component home

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,23 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  showErrorMessage: boolean = false;  // Flag per mostrare il messaggio di errore
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Verifica se il parametro 'error' è presente nell'URL
+    this.route.queryParams.subscribe(params => {
+      if (params['error'] === 'true') {
+        this.showErrorMessage = true;  // Mostra il messaggio di errore
+
+        // Nascondi il messaggio di errore dopo 2 secondi
+        setTimeout(() => {
+          this.showErrorMessage = false;  // Nasconde il messaggio
+        }, 2000);  // Tempo di visualizzazione del popup (2 secondi)
+      }
+    });
+  }
 }
