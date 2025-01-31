eecb4ad7624cf87e752e6d8ec8b6e7d76c4653ed
MattProgrammer98 <iozzia.matteo@gmail.com>
2025-01-31 16:27:15 +0100

    Commit Navbar and index

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  // Definisci eventuali metodi o proprietà, se necessario
 }
