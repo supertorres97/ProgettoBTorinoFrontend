7969a29cd00359724811cf8b51deca7900e6eb05
super_torres97 <supertorres97@gmail.com>
2025-02-28 15:26:48 +0100

    aggiunta paginazione anche ai prodotti per tipo

--- src/app/Components/prodotti-per-tipo/prodotti-per-tipo.component.ts
+++ src/app/Components/prodotti-per-tipo/prodotti-per-tipo.component.ts
@@ -17,6 +17,8 @@ export class ProdottiPerTipoComponent {
     showSearch: boolean = false;
     searchQuery: string = '';
     id:any;
+    page: number = 1;
+    itemsPerPage: number = 9;
 
     constructor(private serv:ProdottiService, private router:Router, private route: ActivatedRoute, private tProdS:TipoProdottoService) { }
   
@@ -42,53 +44,7 @@ export class ProdottiPerTipoComponent {
         });
       });
     }
-/*  
-        this.route.queryParams.subscribe(params => {
-          const nome = params['nome'];
-          if (nome) {
-            this.searchQuery = nome;
-            this.cercaProdotti(nome);
-          } else {
-            this.getAllProdotti(); // Se non c'è parametro, carica tutto
-          }
-        });*/
-  /*
-    cercaProdotti(nome: string): void {
-      if (!nome.trim()) { // Se il nome è vuoto o solo spazi, carica tutti i prodotti
-      this.getAllProdotti();
-      return;
-    }
-  
-    this.serv.getProdottiByNome(nome).subscribe({
-      next: (data: any) => {
-        this.prodotti = data.dati;
-      },
-      error: (err) => {
-        console.error('Errore nella ricerca dei prodotti', err);
-      }
-    });
-  
-    // Aggiorna la URL con il nuovo parametro di ricerca
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: nome ? { nome } : {}, // Se nome è vuoto, rimuove il parametro dalla URL
-      queryParamsHandling: 'merge'
-    });
-    }
-  
-  
-    getAllProdotti(): void {
-      this.serv.listProdotti().subscribe({
-        next: (response: any) => {
-          this.prodotti = response.dati;
-        },
-        error: (err) => {
-          console.error('Errore nel recupero dei prodotti', err);
-          this.prodotti = [];
-        }
-      });
-    }
-*/  
+
     dettagliProdotto(id:number){
       console.log(id);
       this.router.navigate(['/prodotto/', id]);
